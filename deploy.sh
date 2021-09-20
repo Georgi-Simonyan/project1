aa3c90a8af1bb7589f652b5c9dd253201a21c226
Georgi-Simonyan <88677682+Georgi-Simonyan@users.noreply.github.com>
2021-09-20 21:41:29 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,18 @@
 set -e
 PUBLIC_IP=$(curl ipinfo.io/ip)
 AWS_REGION=eu-central-1
-SG_ID=sg-0e36f051e3fe48761
+SG_ID=sg-0c8b28fbae64b1c66
 /usr/local/bin/aws  ec2 authorize-security-group-ingress --region $AWS_REGION --group-id $SG_ID \
   --protocol tcp --port 22 --cidr $PUBLIC_IP/24
 sleep 5
 
-EC2_USERNAME=ec2-user
-EC2_PUBLIC_IP=18.157.74.101
+EC2_USERNAME=ubuntu
+EC2_PUBLIC_IP=3.142.232.170 
 
 #Old EC2_PUBLIC_IP=18.159.135.7
 scp -r -o  StrictHostKeyChecking=no $PWD/* $EC2_USERNAME@$EC2_PUBLIC_IP:~
 ssh -o  StrictHostKeyChecking=no $EC2_USERNAME@$EC2_PUBLIC_IP bash << EOF
-$(aws ecr get-login --no-include-email --region eu-central-1) && \
+$(aws ecr get-login --no-include-email --region us-east-2) && \
 docker-compose up -d
 #&& \
 #docker images | awk '{print $3}' | xargs docker rmi
