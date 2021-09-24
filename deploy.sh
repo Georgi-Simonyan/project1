47f4ff725fb69149101780a271dc3986fa668199
Georgi-Simonyan <88677682+Georgi-Simonyan@users.noreply.github.com>
2021-09-24 18:55:46 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 PUBLIC_IP=$(curl ipinfo.io/ip)
-AWS_REGION=us-east-2
+AWS_REGION=us-east-2c
 SG_ID=sg-0c8b28fbae64b1c66
 /usr/local/bin/aws  ec2 authorize-security-group-ingress --region $AWS_REGION --group-id $SG_ID \
   --protocol tcp --port 22 --cidr $PUBLIC_IP/24
 sleep 5
 
 EC2_USERNAME=ubuntu
-EC2_PUBLIC_IP=3.142.232.170 
+EC2_PUBLIC_IP=3.142.232.170
 
 #Old EC2_PUBLIC_IP=18.159.135.7
 scp -r -o  StrictHostKeyChecking=no $PWD/* $EC2_USERNAME@$EC2_PUBLIC_IP:~
